8ead7498ab38fca9403ec79ba48b5fe6eaa6b25d
Zach Kaigler <zachkaiglerfilm@gmail.com>
2023-03-10 13:45:00 -0500

    added touch support

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import { DndProvider } from 'react-dnd';
 import { HTML5Backend } from 'react-dnd-html5-backend';
-import { convertBoardToMatrix } from './board'
+import { TouchBackend } from 'react-dnd-touch-backend';
+import { isMobile } from 'react-device-detect';
+import { convertBoardToMatrix } from './board';
 import BoardSquareTile from './components/board elements/BoardSquareTile/BoardSquareTile';
 import { useGameController } from './hooks/useGameController/useGameController';
 import './App.scss'
@@ -10,7 +12,7 @@ function App() {
   const boardArray = convertBoardToMatrix(game);
   return (
     <div className="App">
-      <DndProvider backend={HTML5Backend}>
+      <DndProvider backend={isMobile ? TouchBackend : HTML5Backend}>
         <div className='Chess__Board'>
           {boardArray.map((square) => (
             <BoardSquareTile
